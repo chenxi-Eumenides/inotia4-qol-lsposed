db60a75e90d7d59c65155e867f29f76bb1d43775
chenxi-Eumenides <1742041477@qq.com>
2026-09-14 02:05:55 +0800

    chore: 版本号 0.7.5 → 0.7.6（versionCode 185）

--- module/app/build.gradle.kts
+++ module/app/build.gradle.kts
@@ -54,8 +54,8 @@ android {
         applicationId = "com.inotia4.qol"
         minSdk = 30
         targetSdk = 34
-    versionCode = 184
-    versionName = "0.7.5"
+    versionCode = 185
+    versionName = "0.7.6"
 
         externalNativeBuild {
             cmake {
