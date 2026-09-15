2d9ad0a9721f9866a6f360d1b48531d5f196d9e4
chenxi-Eumenides <1742041477@qq.com>
2026-09-15 20:38:50 +0800

    chore: 版本号 0.7.9 → 0.7.10（versionCode 189）

--- module/app/build.gradle.kts
+++ module/app/build.gradle.kts
@@ -54,8 +54,8 @@ android {
         applicationId = "com.inotia4.qol"
         minSdk = 30
         targetSdk = 34
-    versionCode = 188
-    versionName = "0.7.9"
+    versionCode = 189
+    versionName = "0.7.10"
 
         externalNativeBuild {
             cmake {
